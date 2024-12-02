ee54be2fe008cfe06511d48bc4af5342428695df
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2024-12-03 00:17:54 +0100

    day02

---
+++ src/test/kotlin/day02/RedNosedReportsTest.kt
@@ -0,0 +1,18 @@
+package day02
+
+import readResource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RedNosedReportsTest {
+
+    @Test
+    fun testPart1() {
+        assertEquals(2, part1(readResource("day02/input.txt")))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(4, part2(readResource("day02/input.txt")))
+    }
+}
